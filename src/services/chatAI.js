c6ad227d5a042aa74bd7730afc7f88149ed90c37
techieshruti <shrutichandra721@gmail.com>
2026-07-21 21:08:54 +0530

    Debug Vercel env

--- src/services/chatAI.js
+++ src/services/chatAI.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "https://health-companion-ai-5ri9.onrender.com/api";
   console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
 console.log("API_BASE_URL =", API_BASE_URL);
 
